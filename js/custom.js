1846b4402a9506d646ad72544f054a1a471252e6
alamin139 <alamin237112@gmail.com>
2021-10-05 15:12:26 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.banner_slider').slick({
+        autoplay:true,
+        dots:false,
+        arrows:true,
+        slidesToShow: 1,
+        prevArrow:'<i class="fa fa-angle-left baneerPrevArrow"></i>',
+        nextArrow:'<i class="fa fa-angle-right baneerNextArrow"></i>',
+      });
+})
